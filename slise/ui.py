0c9f56d575c0e99086c0cbfc10d637db1d73cb9a
akash <akash0x53s@gmail.com>
2013-09-15 00:37:57 +0530

    got path frm file_chooser, need to display selected image on DrawingArea

--- slise/ui.py
+++ slise/ui.py
@@ -1,10 +1,14 @@
 from gi.repository import Gtk as gtk
+from gi.repository import Gdk as gdk
+from gi.repository import GdkPixbuf
+ 
 import os
 
 class Slise_win:
     
     def __init__(self):
-        ui_file=os.path.dirname(__file__)+"/gui/slise.glade"
+        self.path=None
+        ui_file=os.path.dirname(__file__)+"./gui/slise.glade"
         
         if os.path.exists(ui_file):
             print 'found glade...'
@@ -16,11 +20,45 @@ class Slise_win:
         window=gtk.Window()
         window=builder.get_object('window1')
         window.connect('delete-event',self.onExit)
+        #file chooser dialog box
+        select_file=builder.get_object('file_chooser')
+        select_file.connect('file-set',self.onSelectFile) #on selection of file using FileChooseDialog box it generates "file-set" signal
+        #drawing area
         
-        
-        
+        self.area=builder.get_object('preview')
+        self.area.realize()
+        self.area.connect('draw',self.onExpose)
+                
         window.show_all()
         
+    def onExpose(self,area,cairo):
+        #print dir(area)
+               
+        if self.path is not None:
+            #area.realize()
+            cr=gdk.cairo_create(area.get_window())
+            
+            pix=GdkPixbuf.Pixbuf()
+            pix.new_from_file(self.path)
+            print pix
+            img=gtk.Image()
+            img.set_from_pixbuf(pix)           
+            
+            gdk.cairo_set_source_pixbuf(cr,img.get_pixbuf(),0.0,0.0)
+            cairo.paint()
+            cairo.fill()
+            cairo.destroy()
+                        
+        
+    def onSelectFile(self,event):
+        self.path=event.get_filename()
+        print self.path
+        self.area.queue_draw()
+        
+    @property    
+    def select_file(self):
+        return self.path
+                
     def onExit(self,event,data):
         print 'Bye...',event,data
         gtk.main_quit()
