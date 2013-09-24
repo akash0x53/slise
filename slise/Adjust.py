6cd3ee8132c7c8b6effe3a19e60c5d5edb0d4208
akash <akash0x53s@gmail.com>
2013-09-24 12:31:39 +0530

    Fixed property issues

--- slise/Adjust.py
+++ slise/Adjust.py
@@ -2,31 +2,30 @@
 # 1] if image size > 640x480, resize to 640x480
 # 2] accept only color image (current support)
 import cv2
-import numpy as np
 from slise.slise_exception import SliseException 
 
-class Adjust:
+class Adjust(object):
     def __init__(self):
         self.template=None
-                       
+        
+    @property                   
     def image(self):
         return self.template
-    
-    def set_image(self,img):
-               
+
+    @image.setter
+    def image(self,img):
         if img.shape[2]!=3 or img.dtype!='uint8' or (img.shape[1]<640 or img.shape[1]<480):
             raise SliseException("Image dimension error")
         else:
+            print 'image set'
             self.template=img
     
     def resize(self):
-        print self.template.shape
-        self.template=cv2.resize(self.template,(640,480))
-        return self.template
-    
-    def tohsv(self):
-        self.template=cv2.cvtColor(self.template,cv2.cv.CV_BGR2HSV)
-        return self.template
+        self.resized=cv2.resize(self.template,(640,480))
+            
+    def tohsv(self,img):
+        self.hsv=cv2.cvtColor(img,cv2.cv.CV_BGR2HSV)
+        
     
     @property
     def size(self):
